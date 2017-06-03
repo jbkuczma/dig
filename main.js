c7fcc7fb8d3c4ecf4c0ca07df185ef41033f77b3
James <jbkuczma@buffalo.edu>
2017-06-03 14:34:59 -0400

    login page creation started

--- main.js
+++ main.js
@@ -15,12 +15,18 @@ function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
-  // and load the index.html of the app.
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, 'login.html'),
     protocol: 'file:',
     slashes: true
   }))
+
+  // and load the index.html of the app.
+  // mainWindow.loadURL(url.format({
+  //   pathname: path.join(__dirname, 'index.html'),
+  //   protocol: 'file:',
+  //   slashes: true
+  // }))
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
