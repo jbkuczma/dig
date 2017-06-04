8869b70f9629405c0677733afdbd7cadc2315929
James <jbkuczma@buffalo.edu>
2017-06-04 13:19:03 -0400

    heart svg and styling for a like button

--- src/components/Feed/ImageFeed/image.jsx
+++ src/components/Feed/ImageFeed/image.jsx
@@ -6,14 +6,18 @@ export default class Image extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            user: this.props.user
+            user: this.props.user,
+            imgsrc: "http://assets.fodors.com/destinations/4224/night-aerial-chicago-illinois-usa_main.jpg"
         }
     }
     
     render() {
         return (
-            <div>
-                <p> {this.state.user} </p>
+            <div className="instagramImage">
+                <img src={this.state.imgsrc} />
+                <button id="likeButton">
+                    <img src="./assets/heart.svg" />    
+                </button>
             </div>
         )
     }
