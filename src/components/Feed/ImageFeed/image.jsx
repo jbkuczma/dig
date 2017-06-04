7c7d87f9ba1e373f4a4eef156f719e50dfb37d8c
James <jbkuczma@buffalo.edu>
2017-06-04 16:12:16 -0400

    image feed is retrieved when given usernames (2 most recent photos retrieved). css fixed a bug where photo caption would not wrap

--- src/components/Feed/ImageFeed/image.jsx
+++ src/components/Feed/ImageFeed/image.jsx
@@ -1,24 +1,86 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import request from 'request'
+import _ from 'underscore'
+
+var Client = require('instagram-private-api').V1;
+var device = new Client.Device('user');
+var storage = new Client.CookieFileStorage(__dirname + '/cookies/user.json');
+
+var Promise = require('bluebird');
+
+var userInfo = require('../../../../cookies/info.json')
+
+const username = userInfo['username']
+const password = userInfo['password']
+
 export default class Image extends React.Component {
 
     constructor(props) {
         super(props)
         this.state = {
             user: this.props.user,
-            imgsrc: "http://assets.fodors.com/destinations/4224/night-aerial-chicago-illinois-usa_main.jpg"
+            images: [],
+            ready: false
         }
+    }
+
+    componentWillMount(){
+        this.fetchImages()
+    }
+
+    fetchImages() {
+        Client.Session.create(device, storage, username, password)
+            .then((session) => {
+                return [session, Client.Account.searchForUser(session, this.props.user)]
+            })
+            .spread((session, account) => {
+                let userid = account.id
+                let feed = new Client.Feed.UserMedia(session, userid)
+                return feed.get() /* returns ~12 photos */
+            .then((userFeed) => {
+                let photos = []
+                for(var i = 0; i < 2; i ++){
+                    let url = userFeed[i]._params.images[0].url.split('?')[0]
+                    let id = userFeed[i]._params.id
+                    let caption = userFeed[i]._params.caption
+                    let data = {
+                        url: url, 
+                        id: id,
+                        caption: caption
+                    }
+                    photos.push(data)
+                }
+                this.setState({
+                    images: photos,
+                    ready: true
+                })
+            })
+        })
     }
     
     render() {
-        return (
-            <div className="instagramImage">
-                <img src={this.state.imgsrc} />
-                <button id="likeButton">
-                    <img src="./assets/heart.svg" />    
-                </button>
-            </div>
-        )
+        if(this.state.ready) {
+            return (
+                <div>
+                    {this.state.images.map((photo) => {
+                        return (
+                            <div className="instagramImage" key={photo.id}>
+                                <img src={photo.url} />
+                                <button id="likeButton">
+                                    <img src="./assets/heart.svg" />    
+                                </button>
+                                <span id="imageCaption"> {photo.caption === undefined ? '' : photo.caption} </span>
+                            </div>
+                        )
+                    })}
+                </div>
+            )
+        } else {
+            return (
+                <p> loading </p>
+            )
+        }
     }
 }
